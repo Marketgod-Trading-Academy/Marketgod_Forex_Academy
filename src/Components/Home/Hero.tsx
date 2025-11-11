7e38ddfed0ac95a8233571a76a27fc6d4a1b1b05
Eugene Afriyie <groupeight00@gmail.com>
2025-11-11 16:41:07 +0000

    made the hero font size more reasonable on sm

--- src/Components/Home/Hero.tsx
+++ src/Components/Home/Hero.tsx
@@ -217,15 +217,15 @@ const handleDragEnd = (
                   <span className="font-mono">{timeString}</span>
                 </div>
 
-                <h1 className="text-5xl md:text-7xl font-black tracking-tight leading-tight text-white drop-shadow-2xl">
+                <h1 className="text-4xl md:text-7xl font-black tracking-tight leading-tight text-white drop-shadow-2xl">
                   {currentSlide.title}
                 </h1>
 
-                <h3 className="text-2xl md:text-4xl font-semibold text-white/90 drop-shadow">
+                <h3 className="text-xl md:text-4xl font-semibold text-white/90 drop-shadow">
                   {currentSlide.subtitle}
                 </h3>
 
-                <p className="max-w-3xl mx-auto text-lg md:text-xl leading-relaxed text-white/80 drop-shadow">
+                <p className="max-w-3xl mx-auto text-[1rem] md:text-xl leading-relaxed text-white/80 drop-shadow">
                   {currentSlide.desc}
                 </p>
 
